b838edc52532d1ea2de4cdf105bba5fe16381de1
angmu <62282089+angmu@users.noreply.github.com>
2020-07-31 00:51:39 +0900

    /

--- Ex0303/src/Medic.java
+++
@@ -1,9 +0,0 @@
-
-public class Medic extends GroundUnit  implements Tran{
-	Medic(){
-		super(2);
-	}
-	void move(int x, int y) {}
-	int transit_space=2;
-	String name="Medic";
-}
--- Ex0303/src/Unit.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class Unit {
-	int x,y;
-	String name;
-	int transit_space;
-	abstract void move(int x, int y);
-	void move() {}
-}
